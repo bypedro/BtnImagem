ed6b4737603ce76bf122937a8486f80d02160b3a
bypedro <bypedro12@hotmail.com>
2016-11-27 02:44:32 +0000

    Start

---
+++ BtnImagem/BtnImagem.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BtnImagem
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblTexto = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LblTexto
+        '
+        Me.LblTexto.AutoSize = True
+        Me.LblTexto.Location = New System.Drawing.Point(59, 21)
+        Me.LblTexto.Name = "LblTexto"
+        Me.LblTexto.Size = New System.Drawing.Size(43, 13)
+        Me.LblTexto.TabIndex = 0
+        Me.LblTexto.Text = "TEXTO"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'BtnImagem
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LblTexto)
+        Me.Name = "BtnImagem"
+        Me.Size = New System.Drawing.Size(172, 57)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LblTexto As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ BtnImagem/BtnImagem.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Drawing.Text
+Imports System.Drawing
+
+Public Class BtnImagem
+    Public Property Texto As String = "TEXTO"
+    Public Property CorFundo As Color = Color.White
+    Public Property Imagem As System.Drawing.Bitmap
+
+    Private Sub BtnImagem_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim customfont As New PrivateFontCollection
+        customfont.AddFontFile("C:\maven.ttf") '' Usar Talvez Uma Propriedade
+        LblTexto.Font = New Drawing.Font(customfont.Families(0), 10)
+        LblTexto.Text = Texto
+        Me.BackColor = CorFundo
+        PictureBox1.BackgroundImage = Imagem
+    End Sub
+
+    Private Sub BtnImagem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Click
+
+    End Sub
+
+    Private Sub BtnImagem_Hover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.MouseHover
+
+    End Sub
+
+
+
+
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+        BtnImagem_Click(sender, e)
+    End Sub
+
+    Private Sub LblTexto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LblTexto.Click
+        BtnImagem_Click(sender, e)
+    End Sub
+
+End Class
+
+
