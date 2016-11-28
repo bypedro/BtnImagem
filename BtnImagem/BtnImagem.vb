8b04ef253bbe26fef491e509517e0fb8fb5bb40c
bypedro <bypedro12@hotmail.com>
2016-11-28 09:41:21 +0000

    Adicionado Propriedades
    Removido Imports desnecessários.
    Atualizado Nomes de propriedades para mais facil utilização.
    Adicionado Propriedades  para Tamanho da Imagem e Cor secundária.
    Adicionado evento que muda fundo para cor secundária.

--- BtnImagem/BtnImagem.vb
+++ BtnImagem/BtnImagem.vb
@@ -1,26 +1,23 @@
 ﻿Imports System.Drawing.Text
 Imports System.Drawing
-Imports System.IO
-Imports System.Reflection
 
 Public Class BtnImagem
-    Public Property Texto As String = "TEXTO"
-    Public Property CorFundo As Color = Color.White
-    Public Property Imagem As System.Drawing.Bitmap
-    Public Property TamanhoLetra As Integer = 11
+    Public Property zTexto As String = "TEXTO"
+    Public Property zTamanhoLetra As Integer = 11
+    Public Property zCorFundo As Color = Color.DarkGray
+    Public Property zCorSecundaria As Color = Color.Gray
+    Public Property zImagem As System.Drawing.Bitmap
+    Public Property zComprimentoImagem As Integer = 50
+    Public Property zAlturaImagem As Integer = 50
+
 
     Private Sub BtnImagem_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        LblTexto.Font = CustomFont.GetInstance(TamanhoLetra, FontStyle.Bold) 'Fonte Personalizada não tem um style.
-        Me.BackColor = CorFundo
-        PictureBox1.BackgroundImage = Imagem
-        LblTexto.Text = Texto
-    End Sub
-
-    Private Sub BtnImagem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Click
-        MsgBox("LOL")
-    End Sub
-
-    Private Sub BtnImagem_Hover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.MouseHover
+        LblTexto.Font = CustomFont.GetInstance(zTamanhoLetra, FontStyle.Bold) 'Fonte Personalizada não tem um style.
+        LblTexto.Text = zTexto
+        Me.BackColor = zCorFundo
+        PictureBox1.BackgroundImage = zImagem
+        PictureBox1.Width = zComprimentoImagem
+        PictureBox1.Height = zAlturaImagem
 
     End Sub
 
@@ -30,6 +27,31 @@ Public Class BtnImagem
 
     Private Sub LblTexto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LblTexto.Click
         BtnImagem_Click(sender, e)
+    End Sub
+
+
+
+
+    Private Sub BtnImagem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Click
+
+    End Sub
+
+    ''Style
+    Private Sub BtnImagem_Hover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.MouseHover
+        Me.BackColor = zCorSecundaria
+    End Sub
+
+    Private Sub BtnImagem_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.MouseLeave
+        Me.BackColor = zCorFundo
+    End Sub
+
+
+    Private Sub PictureBox1_hover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.MouseHover
+        Me.BackColor = zCorSecundaria
+    End Sub
+
+    Private Sub LblTexto_Hover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LblTexto.MouseHover
+        Me.BackColor = zCorSecundaria
     End Sub
 
 End Class
